88f6e7158951c2ad0979e1ae20e4a5d20bb4b377
Amari Ellington <amari.l.ellington@gmail.com>
2019-01-02 22:33:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ function logWhisper(string) {
 logWhisper(string)
 
 function sayHiToGrandma(string){
-  if (string.toUpperCase === string) {
-    return "YES INDEED!"
-  }
-else if (string === "I love you,  Gradma.") {
-  return "I love you, too."
-}
-else {
-  return ("I can't hear you")
-}
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
 }
