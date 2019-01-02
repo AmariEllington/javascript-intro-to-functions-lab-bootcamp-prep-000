8dd90e1b9e4c307f4b0a9d97a9bb98c8857b1fa4
Amari Ellington <amari.l.ellington@gmail.com>
2019-01-02 22:20:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,5 @@ function logWhisper(string) {
 logWhisper(string)
 
 function sayHiToGrandma(string){
-if sayHiToGrandma(string) === lowercase
   return "I can't hear you!"
-else if
-  return "YES INDEED!"
 }
